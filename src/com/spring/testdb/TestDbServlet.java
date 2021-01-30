ae30d0c777cf4b76859d76e12ab1c44214b24dd8
venu gopal reddy <yvgr00@gmail.com>
2021-01-30 16:44:52 -0500

    created project setup

---
+++ src/com/spring/testdb/TestDbServlet.java
@@ -0,0 +1,53 @@
+package com.spring.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String user = "springstudent";
+		String password = "springstudent";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		try {
+			
+			PrintWriter out = response.getWriter();
+			
+			out.println("connecting to database: "+jdbcUrl);
+			
+			Class.forName(driver);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl,user,password);
+			
+			out.println("!Successful");
+			
+			myConn.close();
+			
+		}catch(Exception exc){
+			exc.printStackTrace();
+			throw new ServletException(exc);
+			
+		}
+	}
+
+}
